3e37588b7bdbdd22e7b03fdc16d2f19ecfa32152
adrian <adriangallanoengr@gmail.com>
2022-02-07 10:31:11 +0800

    Clean code and Write more Test

--- solver.js
+++ solver.js
@@ -40,17 +40,13 @@ function isEmpty(grid){ // returns address [y,x]
 
 function solve(grid){
     let find = isEmpty(grid)
-
-
-    //fix this
     if(!find){
-        return [...grid];
+        return grid;
     }else{
         let [row,col] = find;
         for(let n = 1; n < 10; n++){
             if(isValid(grid, find, n)){
                 grid[row][col] = n
-    
                 if(solve(grid)){
                     return grid;
                 }
@@ -62,20 +58,5 @@ function solve(grid){
     return false;
 }
 
-function printBoard(grid){
-    for(let y = 0; y < grid.length; y++){
-        if(y % 3 == 0){
-            console.log("--------------------")
-            console.log(`${grid[y].slice(0,3).join(' ')} | ${grid[y].slice(3,6).join(' ')} | ${grid[y].slice(6,9).join(' ')}`)
-        }else{
-            console.log(`${grid[y].slice(0,3).join(' ')} | ${grid[y].slice(3,6).join(' ')} | ${grid[y].slice(6,9).join(' ')}`)
-        }
-    }
-} 
-
-const grid = [[5,3,4,6,7,8,9,1,2], [6,7,2,1,9,5,3,4,8],[1,9,8,3,4,2,5,6,7], [8,5,9,7,6,1,4,2,3],[4,2,6,8,5,3,7,9,1], [7,1,3,9,2,4,8,5,6],[9,6,1,5,3,7,2,8,4], [2,8,7,4,1,9,6,3,5],[3,4,5,2,8,6,1,7,9]]
-
-let s = solve(grid);
-console.log(s)
 
 module.exports = solve;
