e5def37159a4752cd4100603884814fc8207be8e
adrian <adriangallanoengr@gmail.com>
2022-02-07 09:41:30 +0800

    Rewrite test case Finish project

--- solver.js
+++ solver.js
@@ -17,7 +17,7 @@ function isValid(grid, currAddress, currNum){
     let startCol =  col - col % 3;
     for(let y = 0; y < 3; y++){
         for(let x = 0; x < 3; x++){
-            if(grid[startRow][startRow][startCol] == currNum){
+            if(grid[startRow+y][startCol+x] == currNum){
                 return false
             }
         }
@@ -41,41 +41,41 @@ function isEmpty(grid){ // returns address [y,x]
 function solve(grid){
     let find = isEmpty(grid)
 
+
+    //fix this
     if(!find){
-        return true;
+        return [...grid];
     }else{
         let [row,col] = find;
-        for(let n = 1; n <= 9; n++){
+        for(let n = 1; n < 10; n++){
             if(isValid(grid, find, n)){
                 grid[row][col] = n
-
+    
                 if(solve(grid)){
-                    return true;
+                    return grid;
                 }
-            }else{
-                grid[row][col] = 0
             }
+            grid[row][col] = 0
         }
     }
+
     return false;
 }
 
+function printBoard(grid){
+    for(let y = 0; y < grid.length; y++){
+        if(y % 3 == 0){
+            console.log("--------------------")
+            console.log(`${grid[y].slice(0,3).join(' ')} | ${grid[y].slice(3,6).join(' ')} | ${grid[y].slice(6,9).join(' ')}`)
+        }else{
+            console.log(`${grid[y].slice(0,3).join(' ')} | ${grid[y].slice(3,6).join(' ')} | ${grid[y].slice(6,9).join(' ')}`)
+        }
+    }
+} 
 
+const grid = [[5,3,4,6,7,8,9,1,2], [6,7,2,1,9,5,3,4,8],[1,9,8,3,4,2,5,6,7], [8,5,9,7,6,1,4,2,3],[4,2,6,8,5,3,7,9,1], [7,1,3,9,2,4,8,5,6],[9,6,1,5,3,7,2,8,4], [2,8,7,4,1,9,6,3,5],[3,4,5,2,8,6,1,7,9]]
 
-
-
-
-const grid = 
-[[0,2,0,0,0,4,3,0,0],
-[9,0,0,0,2,0,0,0,8], 
-[0,0,0,6,0,9,0,5,0], 
-[0,0,0,0,0,0,0,0,1], 
-[0,7,2,5,0,3,6,8,0], 
-[6,0,0,0,0,0,0,0,0], 
-[0,8,0,2,0,5,0,0,0], 
-[1,0,0,0,9,0,0,0,3], 
-[0,0,9,8,0,0,0,6,0]]
-
-let s = solve(grid)
+let s = solve(grid);
 console.log(s)
-console.log(...grid)
+
+module.exports = solve;
